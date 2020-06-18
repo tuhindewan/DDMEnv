76e564167c7ba4a842dc8a57f700e13cb27bf8dc
tuhindewan <tuhinsshadow@gmail.com>
2020-06-18 15:21:00 +0600

    FilesUpload app create

--- filesUpload/views.py
+++ filesUpload/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    dictionary = {
+        'title': 'Index Page'
+    }
+    return render(request, 'filesUpload/index.html', context=dictionary)
